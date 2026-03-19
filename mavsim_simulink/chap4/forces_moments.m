29904a8e140d9277392c7ca6fb8515f2efc0a7b6
Krishna Kalpeshkumar Bhatt <23B0040@iitb.ac.in>
2026-03-20 02:22:40 +0530

    Update forces_moments.m

--- mavsim_simulink/chap4/forces_moments.m
+++ mavsim_simulink/chap4/forces_moments.m
@@ -102,10 +102,11 @@ function out = forces_moments(x, delta, wind, P)
     end
     
     % Propulsion Force (Simplified model based on textbook)
-    % Note: If Va > k_motor * delta_t, thrust becomes negative (drag). 
-    fp_x = 0.5 * P.rho * P.S_prop * P.C_prop * ((P.k_motor * delta_t)^2 - Va^2);
+    fp_x = P.T_max * delta_t;
     fp_y = 0;
     fp_z = 0;
+
+
     
     % Total Forces
     Force(1) = fg_x + fa_x + fp_x;
@@ -117,7 +118,7 @@ function out = forces_moments(x, delta, wind, P)
         Torque(1) = 0; Torque(2) = 0; Torque(3) = 0;
     else
         % Roll Moment (l) - Includes adverse motor torque
-        Torque(1) = qbar * P.S_wing * P.b * (P.C_ell_0 + P.C_ell_beta * beta + P.C_ell_p * (P.b / (2*Va)) * p + P.C_ell_r * (P.b / (2*Va)) * r + P.C_ell_delta_a * delta_a + P.C_ell_delta_r * delta_r) - P.k_T_P * (P.k_Omega * delta_t)^2;
+        Torque(1) = qbar * P.S_wing * P.b * (P.C_ell_0 + P.C_ell_beta * beta + P.C_ell_p * (P.b / (2*Va)) * p + P.C_ell_r * (P.b / (2*Va)) * r + P.C_ell_delta_a * delta_a + P.C_ell_delta_r * delta_r);
         
         % Pitch Moment (m)
         Torque(2) = qbar * P.S_wing * P.c * (P.C_m_0 + P.C_m_alpha * alpha + P.C_m_q * (P.c / (2*Va)) * q + P.C_m_delta_e * delta_e);
