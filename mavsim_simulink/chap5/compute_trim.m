935cccdc537ace09b7b380a9a4f62476312e6c34
Krishna Kalpeshkumar Bhatt <23B0040@iitb.ac.in>
2026-03-21 23:14:27 +0530

    Refactor compute_trim.m for 13-state model

--- mavsim_simulink/chap5/compute_trim.m
+++ mavsim_simulink/chap5/compute_trim.m
@@ -1,58 +1,60 @@
-% compute trim conditions using 'mavsim_trim.slx'
-gamma = 0*pi/180;   % desired flight path angle (radians)
-R     = 10000000000;  % desired radius (m): (+) right orbit, (-) left orbit
-Va    = 25;
+%% compute_trim.m
+clear; clc;
 
-% set initial conditions (12 Euler states: pn,pe,pd,u,v,w,phi,theta,psi,p,q,r)
-x0 = [0; 0; -200; Va; 0; 0; 0; 0; 0; 0; 0; 0];
-% specify which states to hold equal to the initial conditions
-ix = [];
+aerosonde_parameters;
 
-% specify initial inputs
-u0 = [...
-    0;...  % 1 - delta_e
-    0;...  % 2 - delta_a
-    0;...  % 3 - delta_r
-    1;...  % 4 - delta_t
-    ];
-% specify which inputs to hold constant
+gamma = 0*pi/180;
+R     = 10000000000;
+Va    = 200;
+
+%% --- Build 12-state Euler initial condition ---
+x0_12 = [0; 0; -200; Va; 0; 0; 0; 0; 0; 0; 0; 0];
+
+%% --- Convert to 13-state quaternion for Simulink ---
+q0 = Euler2Quaternion(x0_12(7), x0_12(8), x0_12(9));    % ← 3 separate args
+x0_13 = [x0_12(1:6); q0; x0_12(10:12)];
+
+%% --- Inputs / outputs ---
+u0 = [0; 0; 0; 1];
 iu = [];
-
-% define constant outputs
-y0 = [...
-    Va;...  % 1 - Va
-    0;...   % 2 - alpha
-    0;...   % 3 - beta
-    ];
-% specify which outputs to hold constant
+ix = [];
+y0 = [Va; 0; 0];
 iy = [1, 3];
 
-% define constant derivatives (12 entries matching 12-state Euler model)
-dx0 = [0; 0; -Va*sin(gamma); 0; 0; 0; 0; 0; 0; 0; 0; 0];
-%      pn  pe  pd            u  v  w  phi th  psi p  q  r
-% pd_dot = -Va*sin(gamma) because pd is positive DOWN, altitude h = -pd
-% so climbing (gamma>0) → pd decreasing → pd_dot < 0
+%% --- Derivatives (13-state) ---
+dx0_13 = [0; 0; -Va*sin(gamma); 0; 0; 0; 0; 0; 0; 0; 0; 0; 0];
+if R ~= Inf, dx0_13(13) = Va*cos(gamma)/R; end
 
-if R ~= Inf, dx0(9) = Va*cos(gamma)/R; end  % 9 - psidot
-% specify which derivatives to hold constant in trim algorithm
-idx = [3; 4; 5; 6; 7; 8; 9; 10; 11; 12];
+idx = [3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13];
 
-% compute trim conditions
-[x_trim, u_trim, y_trim, dx_trim] = trim('mavsim_trim', x0, u0, y0, ix, iu, iy, dx0, idx);
+%% --- Run trim ---
+[x_trim13, u_trim, y_trim, dx_trim13] = ...
+    trim('mavsim_trim', x0_13, u0, y0, ix, iu, iy, dx0_13, idx);
 
-% check to make sure that the linearization worked (should be small)
-norm(dx_trim(3:end) - dx0(3:end))
+%% --- Convert result back to 12-state Euler ---
+[phi, theta, psi] = Quaternion2Euler(x_trim13(7:10));
+x_trim = [x_trim13(1:6); phi; theta; psi; x_trim13(11:13)];
 
-% set initial conditions to trim conditions
+%% --- Sanity check ---
+dx_trim12 = [dx_trim13(1:6); zeros(3,1); dx_trim13(11:13)];
+dx0_12    = [dx0_13(1:6);    zeros(3,1); dx0_13(11:13)];
+fprintf('Trim residual: %.6e\n', norm(dx_trim12(3:end) - dx0_12(3:end)));
+
+%% --- Set MAV initial conditions ---
 MAV.pn0    = 0;
 MAV.pe0    = 0;
 MAV.pd0    = -200;
-MAV.u0     = x_trim(4);   % u at trim
-MAV.v0     = x_trim(5);   % v at trim
-MAV.w0     = x_trim(6);   % w at trim
-MAV.phi0   = x_trim(7);   % roll angle at trim
-MAV.theta0 = x_trim(8);   % pitch angle at trim
-MAV.psi0   = x_trim(9);   % yaw angle at trim
-MAV.p0     = x_trim(10);  % roll rate at trim
-MAV.q0     = x_trim(11);  % pitch rate at trim
-MAV.r0     = x_trim(12);  % yaw rate at trim
+MAV.u0     = x_trim(4);
+MAV.v0     = x_trim(5);
+MAV.w0     = x_trim(6);
+MAV.phi0   = x_trim(7);
+MAV.theta0 = x_trim(8);
+MAV.psi0   = x_trim(9);
+MAV.p0     = x_trim(10);
+MAV.q0     = x_trim(11);
+MAV.r0     = x_trim(12);
+
+fprintf('Trim complete:\n');
+fprintf('  alpha   = %.4f deg\n', y_trim(2)*180/pi);
+fprintf('  theta   = %.4f deg\n', x_trim(8)*180/pi);
+fprintf('  delta_e = %.4f  delta_t = %.4f\n', u_trim(1), u_trim(4));
